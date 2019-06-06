d4abf6a27b3172d2ce2162f06312268b8faf06a2
Chaitra Dangat <chaitradangat084@gmail.com>
2019-06-06 14:09:59 +0530

    added login viewmodel

---
+++ AspMvcTemplates/Models/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace AspMvcTemplates.Models
+{
+    public class LoginViewModel
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string Role { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
