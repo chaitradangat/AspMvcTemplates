1af94e83925bf0ec26d83638199bd6b48fcc8fca
Chaitra Dangat <chaitradangat084@gmail.com>
2019-06-06 08:52:06 +0530

    added login get and post

--- AspMvcTemplates/Controllers/LoginController.cs
+++ AspMvcTemplates/Controllers/LoginController.cs
@@ -1,13 +1,27 @@
 ﻿using System.Web;
 using System.Web.Mvc;
+using AspMvcTemplates.Models;
+
 
 namespace AspMvcTemplates.Controllers
 {
     public class LoginController : Controller
     {
+        [HttpGet]
         public ActionResult Validate()
+        {
+            ViewBag.Title = "Validate";
+
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Validate(LoginModel loginModel)
         {
             return View();
         }
+
+
     }
 }
