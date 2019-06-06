c99d716e9efaee5edaea35f1078f1f63318d46a1
Chaitra Dangat <chaitradangat084@gmail.com>
2019-06-06 08:49:22 +0530

    Simple Login Model

---
+++ AspMvcTemplates/Models/LoginModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AspMvcTemplates.Models
+{
+    public class LoginModel
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string Role { get; set; }
+    }
+}
