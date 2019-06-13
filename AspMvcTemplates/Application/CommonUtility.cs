4c4da33fee1a9a227d6b6be06716e948562e4a68
chaitra.dangat <chaitradangat084@gmail.com>
2019-06-13 18:57:58 +0530

    common utility class across the application

---
+++ AspMvcTemplates/Application/CommonUtility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspMvcTemplates.Application
+{
+    public class CommonUtility
+    {
+        public bool ValidateLoginCredentials(string username,string password,ref string userid)
+        {
+            if (username == "admin" && password == "admin")
+            {
+                userid = "1";
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+    }
+}
